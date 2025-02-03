353596f4b5c8e42dbf603ce7c6eebe53f93c0999
youer0219 <xlm20040219@qq.com>
2025-02-03 15:25:13 +0800

    网格横板寻路：实现根据高度跳跃与修复最后一点缺少Y值判定的BUG

--- 网格横板寻路/player/player.gd
+++ 网格横板寻路/player/player.gd
@@ -14,19 +14,30 @@ func _process(delta):
 
 func move(delta:float):
 	fall(delta)
-
+	if target_path:
+		print(target_path)
 	if target_path and not target_path.is_empty():
 		
 		plat_move(target_path[0])
 		
-		if target_path[0].y < curr_cell.y:
-			jump()
+		if target_path[0].y < curr_cell.y and is_on_floor():
+			var jump_cells:int = abs(target_path[0].y - curr_cell.y)
+			for num in range(target_path.size()):
+				if num == 0:
+					continue
+				if target_path[num].y < target_path[num-1].y:
+					print("(abs(target_path[num].y - target_path[0].y) + 1: ",(abs(target_path[num].y - target_path[0].y) + 1))
+					jump_cells = abs(target_path[num].y - target_path[0].y) + 1
+				else:
+					break
+			print("jump_cells: ",jump_cells)
+			jump(jump_cells)
 		
 		if curr_cell == target_path[0] and target_path.size() > 1:
 			target_path.remove_at(0)
 		
 		if target_path.size() == 1:
-			if abs(position.x - map_to_local(target_path[0]).x) < 1:
+			if abs(position.x - map_to_local(target_path[0]).x) < 1 and target_path[0].y == curr_cell.y:
 				target_path.remove_at(0)
 				velocity.x = 0
 	
@@ -40,10 +51,16 @@ func do_find_path():
 		var calculated_path = grid.get_true_id_path(start_coord,target_coord)
 		if calculated_path.size() > 0:
 			target_path = calculated_path
+			print("target_path: ",target_path)
 
 func jump(jump_cells = 1):
-	if is_on_floor():
-		velocity.y = -400
+	# 计算所需跳跃高度（像素）
+	var jump_height = 16 * (jump_cells) * 1.1  # 1.2倍余量保证跨格
+	
+	# 根据自由落体公式计算初速度：v0 = sqrt(2 * g * h)
+	var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	var initial_velocity := -sqrt(2 * gravity * jump_height)
+	velocity.y = initial_velocity
 
 func fall(delta:float):
 	if !is_on_floor():
