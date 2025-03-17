fccf5671d2dbfb7ddbeb0c0274b395bfa2810c6c
youer0219 <xlm20040219@qq.com>
2025-03-17 21:05:03 +0800

    PVZR寻路：添加新的点类型

--- PVZR_PathFinderDemo/path_finder.gd
+++ PVZR_PathFinderDemo/path_finder.gd
@@ -6,14 +6,18 @@ class_name PathFinder
 ## 2.提供对路径的处理
 	## 2.1 路径节点数量优化，保留关键节点
 	## 2.2 传入传出都采取全局坐标以兼容
-	## 2.3 寻路路径起点终点检查更加严格
-## 3.特殊：对于“水线”下的节点，将其映射到第一个平台点上进行寻路
+	## 2.3 寻路路径起点终点检查更加严格。起点、终点的映射需要单独的处理逻辑，不仅仅是水线下。
+## 3.特殊：对于“水线”下的节点，将其映射到第一个平台点上进行寻路(OK)
+## 4.添加了一个wall-edge使之更加偏向与跟随建筑移动
 
 const ENTITY_HEIGHT := 2
-const PLATFROM_POINT_WEIGHT := 1
-const AIR_POINT_WEIGHT := 100
 const VECTOR2I_NULL := Vector2(-1,-1)
 const MAP_HEIGHT := 24
+
+const PLATFROM_POINT_WEIGHT := 1
+const WALL_EDGE_POINT_WEIGHT := 10
+const AIR_POINT_WEIGHT := 20
+
 
 @export var map:TileMapLayer
 
@@ -50,6 +54,10 @@ func update_points():
 			elif is_platform_cell(cell):
 				astar.set_point_weight_scale(cell,PLATFROM_POINT_WEIGHT)
 				var new_point = ShowPointPath.CreatePathPoint(map.to_global(map.map_to_local(cell)),Color.BLACK)
+				add_child(new_point)
+			elif is_wall_edge_cell(cell):
+				astar.set_point_weight_scale(cell,WALL_EDGE_POINT_WEIGHT)
+				var new_point = ShowPointPath.CreatePathPoint(map.to_global(map.map_to_local(cell)),Color.RED)
 				add_child(new_point)
 			else:
 				astar.set_point_weight_scale(cell,AIR_POINT_WEIGHT)
@@ -108,6 +116,9 @@ func is_platform_cell(cell:Vector2i)->bool:
 		return true
 	
 	return not get_used_cells().has(cell) and get_used_cells().has(cell + Vector2i.DOWN)
+
+func is_wall_edge_cell(cell:Vector2i)->bool:
+	return get_used_cells().has(cell + Vector2i.RIGHT) or get_used_cells().has(cell + Vector2i.LEFT)
 
 func get_used_cells()->Array[Vector2i]:
 	return map.get_used_cells()
